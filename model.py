7610994d89eb6739f6509743e008a1c38c216e36
pranav <pranavpandey2511@gmail.com>
2020-06-08 10:17:38 +0530

    Modular with N and k

--- model.py
+++ model.py
@@ -1,26 +1,38 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import torch.nn.init as init
+
+def conv_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.xavier_uniform_(m.weight, gain=np.sqrt(2))
+        init.constant_(m.bias, 0)
+    elif classname.find('BatchNorm') != -1:
+        init.constant_(m.weight, 1)
+        init.constant_(m.bias, 0)
+
+
 class BasicBlock(nn.Module):
 
-    def __init__(self, in_feat_maps, out_feat_maps, stride = 1, width_factor = 1, equate_planes = None):
+    def __init__(self, in_feat_maps, out_feat_maps, stride = 1, width_factor = 1,  dropout_rate = 0.3, skip_conn_downsample = None):
 
         super(BasicBlock, self).__init__()
         self._bn1 = nn.BatchNorm2d(num_features = in_feat_maps)
         self._relu1 = nn.ReLU()
-        self._conv1 = nn.Conv2d(in_channels = in_feat_maps, out_channels = out_feat_maps * width_factor, stride = stride, kernel_size = 3, padding = 1)
+        self._conv1 = nn.Conv2d(in_channels = in_feat_maps, out_channels = out_feat_maps * width_factor, stride = 1, kernel_size = 3, padding = 1)
 
-        self._drop1 = nn.Dropout2d(p=0.3)
+        self._drop1 = nn.Dropout2d(p=dropout_rate)
 
         self._bn2 = nn.BatchNorm2d(num_features = out_feat_maps * width_factor)
         self._relu2 = nn.ReLU()
         self._conv2 = nn.Conv2d(in_channels = out_feat_maps * width_factor, out_channels = out_feat_maps * width_factor, stride = stride, kernel_size = 3, padding = 1)
-        self.equate_planes = equate_planes
+        self._skip_conn_downsample = skip_conn_downsample
 
 
     def forward(self, x):
         residual = x
-        #print("Residual shape:", residual.shape)
+        # print("Residual shape:", residual.shape)
         x = self._bn1(x)
         x = self._relu1(x)
         x = self._conv1(x)
@@ -28,34 +40,41 @@ class BasicBlock(nn.Module):
         x = self._bn2(x)
         x = self._relu2(x)
         x = self._conv2(x)
-        #print("X shape:", x.shape)
-        if self.equate_planes:
-            residual = self.equate_planes(residual)
-        x += residual
+        # print("X shape:", x.shape)
+        if self._skip_conn_downsample:
+            x += self._skip_conn_downsample(residual)
 
         return x
 
-def _make_layer(in_feat_maps, out_feat_maps, width_factor = 10):
-    equate_planes = None 
-    if in_feat_maps != out_feat_maps * width_factor:
-       equate_planes = nn.Sequential(nn.Conv2d(in_feat_maps, out_feat_maps * width_factor, kernel_size=1, stride=1), nn.BatchNorm2d(out_feat_maps * width_factor))
-    layer = BasicBlock(in_feat_maps, out_feat_maps, stride = 1, width_factor = width_factor, equate_planes = equate_planes)
+def _make_group(in_feat_maps, out_feat_maps, N, stride =1, width_factor = 10, dropout_rate = 0.3):
+    layers = []
+    for i in range(N):
+        # print(in_feat_maps)
+        skip_conn_downsample = None
 
-    return layer
+        if (stride != 1 or in_feat_maps != out_feat_maps * width_factor):
+            skip_conn_downsample = nn.Conv2d(in_feat_maps, out_feat_maps * width_factor, kernel_size=1, stride=stride)
 
+        layers.append(BasicBlock(in_feat_maps, out_feat_maps, stride = stride, width_factor = width_factor, dropout_rate = dropout_rate, skip_conn_downsample = skip_conn_downsample))
+        # print("Block appended")
+        in_feat_maps = out_feat_maps * width_factor
+        stride = 1
+    block = nn.Sequential(*layers)
 
+    return block
 
 class WideResNet(nn.Module):
 
-    def __init__(self, width_factor = 10):
+    def __init__(self, dropout_rate, num_classes, width_factor = 10, N = 1):
         super(WideResNet, self).__init__()
-        self._conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, stride = 1)
-        self._res_block1 = _make_layer(in_feat_maps = 16, out_feat_maps = 16, width_factor = width_factor)
-        self._res_block2 = _make_layer(in_feat_maps = 16 * width_factor,  out_feat_maps = 32, width_factor = width_factor)
-        self._res_block3 = _make_layer(in_feat_maps = 32 * width_factor,  out_feat_maps = 64, width_factor = width_factor)
+        self._conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, stride = 1, padding = 1)
+        self._res_block1 = _make_group(in_feat_maps = 16, out_feat_maps = 16, N=N, stride = 1, width_factor = width_factor, dropout_rate = dropout_rate)
+        self._res_block2 = _make_group(in_feat_maps = 16 * width_factor,  out_feat_maps = 32, N=N, stride=2, width_factor = width_factor, dropout_rate = dropout_rate)
+        self._res_block3 = _make_group(in_feat_maps = 32 * width_factor,  out_feat_maps = 64, N=N, stride=2, width_factor = width_factor, dropout_rate = dropout_rate)
         self._avg_pool = nn.AvgPool2d(kernel_size = 8)
+        self._bn1 = nn.BatchNorm2d(num_features = 64 * width_factor)
         self._relu = nn.ReLU()
-        self._linear = nn.Linear(5760, 10)        
+        self._linear = nn.Linear(640, num_classes)
 
 
     def forward(self, x):
@@ -63,9 +82,9 @@ class WideResNet(nn.Module):
         x = self._res_block1(x)
         x = self._res_block2(x)
         x = self._res_block3(x)
+        x = self._relu(self._bn1(x))
         x = self._avg_pool(x)
-        x = self._relu(x)
-        x = torch.flatten(x, 1)
+        x = x.view(x.size(0), -1)
         output = self._linear(x)
 
         return output
--- train.py
+++ train.py
@@ -3,36 +3,62 @@ import torch.nn as nn
 import torchvision
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
 from model import WideResNet
+from model import conv_init
 import torchvision.transforms as transforms
+import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
+import argparse
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='Initial learning_rate')
+parser.add_argument('--gamma', default=0.2, type=int, help='learning rate decay factor')
+parser.add_argument('--depth', default=28, type=int, help='depth of model')
+parser.add_argument('--width_factor', default=8, type=int, help='width factor (k) to use for the model')
+parser.add_argument('--dropout_rate', default=0.3, type=float, help='dropout_rate')
+parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
+parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
+args = parser.parse_args()
+
+
+# N is the number of times each block is repeated
+N = int((args.depth - 4)/6) # There are 3 conv blocks, each having 2 conv layers -> 6 total conv layers
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 # Assuming that we are on a CUDA machine, this should print a CUDA device:
-
 print(device)
 
-net = WideResNet()
+# Load the network and initalise the parameters
+net = WideResNet(args.dropout_rate, num_classes = 10, width_factor = 10, N = N)
+net.apply(conv_init)
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Mean & std pre-processsing gives better results as mentioned in the paper (below settings are for cifar 10)
+train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+     transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+])
 
-train_set = CIFAR10(root='./data', train = True, download = True, transform=transform)
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True, drop_last=True, num_workers=2) 
 
-test_set = CIFAR10(root='./data', train = False, download = True, transform=transform)
-test_loader = DataLoader(test_set, batch_size=64, shuffle=True, drop_last=True, num_workers=2) 
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+])
+
+print("============[Phase 1: Data Preperation]=============")
+# Batch size of 128 is used in the paper
+train_set = CIFAR10(root='./data', train = True, download = True, transform=train_transform)
+train_loader = DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, num_workers=2)
+print("| Performing Mean|STD pre-processing on data")
+test_set = CIFAR10(root='./data', train = False, download = True, transform=test_transform)
+test_loader = DataLoader(test_set, batch_size=128, shuffle=True, drop_last=True, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 # functions to show an image
-
-
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
@@ -49,16 +75,21 @@ images, labels = dataiter.next()
 # print labels
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-import torch.optim as optim
-
+# Define the loss function and Optimizer
+num_epochs = 200
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9,  weight_decay=5e-4, nesterov=True)
+scheduler = MultiStepLR(optimizer, milestones=[60, 120, 160], gamma=0.2)
 
+
+print("=============[Phase 3 : Model Training]==================")
+# Push the model to the GPU
 net.to(device)
-for epoch in range(100):  # loop over the dataset multiple times
+
+for epoch in range(num_epochs):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
+    for i, data in enumerate(train_loader):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
         inputs = inputs.to(device)
@@ -74,17 +105,18 @@ for epoch in range(100):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 0:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+        if i % 200 == 0:    # print every 200 mini-batches
+            print('[%d, %5d] loss: %.5f' %
+                  (epoch + 1, i + 1, running_loss / 200))
             running_loss = 0.0
+    scheduler.step()
 
 print('Finished Training')
 
-PATH = './cifar_net.pth'
+PATH = 'drive/My Drive/wide_resnet_cifar10.pth'
 torch.save(net.state_dict(), PATH)
 
-
+net.eval()
 correct = 0
 total = 0
 with torch.no_grad():
@@ -99,5 +131,3 @@ with torch.no_grad():
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
-
-
